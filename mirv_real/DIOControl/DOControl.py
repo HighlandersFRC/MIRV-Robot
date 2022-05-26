01905ba08c48dd02321cb59b7a3074b433d02348
lukesta182 <lskhan@xemaps.com>
2022-05-26 12:02:27 -0600

    Added low level dio control

---
+++ mirv_real/DIOControl/DOControl.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import time
+import os
+import subprocess
+import rospy
+from std_msgs.msg import String
+
+# Pin Definitions
+DO0=320
+DO1=321
+DO2=322
+DO3=323
+outputsDict = {"DO0": DO0, "DO1": DO1, "DO2": DO2, "DO3": DO3}
+def digitalWrite(pin, state):
+    print("tunring pin {} to {}".format(pin, state))
+    subprocess.call(["echo {} >/sys/class/gpio/gpio{}/value".format(state, pin)], shell=True)
+def callback(data):
+    inputData = data.split(",", 1)
+    digitalWrite(outputsDict[inputData[0]], inputData[1])
+def listen():
+    rospy.Subscriber("DIOControl", String, callback)
+    rospy.spin()
+
+def main():
+    rospy.init_node("DIOController", anonymous=True)
+    listen()
+if __name__ == '__main__':
+    main()
