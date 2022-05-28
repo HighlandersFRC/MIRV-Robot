021bb6d5a0dc745548086218c47ab088fbedf0dc
lukesta182 <lskhan@xemaps.com>
2022-05-28 15:03:15 -0600

    added pin init in start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,18 @@ sudo ip link set can0 down
 sudo ip link set can0 type can bitrate 1000000
 sudo ifconfig can0 up
 sudo ifconfig can0 txqueuelen 1000
+function exposeDI() {
+    echo $1 >/sys/class/gpio/export 2>/dev/null
+    echo in >/sys/class/gpio/gpio$1/direction
+}
+function exposeDO() {
+    echo $1 >/sys/class/gpio/export 2>/dev/null
+    echo out >/sys/class/gpio/gpio$1/direction
+}
+exposeDI 325
+exposeDI 331
+exposeDI 326
+exposeDO 320
+exposeDO 321
+exposeDO 322
+exposeDO 323
