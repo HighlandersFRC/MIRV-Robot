b7906f50d178f57f5bc64a7f495c3309f685f674
Paul-Mick <paulmickrobotics@gmail.com>
2022-06-07 11:50:37 -0600

    Encoder based drive odometry

--- mirv_control/JoystickDrive/JoystickDrive.py
+++ mirv_control/JoystickDrive/JoystickDrive.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 
 # Simple Ros Module to read in Joystick values and publish them to the mirv_drive topic
@@ -7,13 +7,14 @@
 
 import rospy
 from std_msgs.msg import String
+import sys
 
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('JoystickDrive', anonymous = True)
     rate = rospy.Rate(10) # 10 Hz
     while not rospy.is_shutdown():
-        data = 'Hello World'
+        data = str(sys.version)
         rospy.loginfo(data)
         pub.publish(data)
         rate.sleep()
@@ -22,5 +23,5 @@ if __name__ == '__main__':
     try:
         talker()
     except rospy.ROSInterruptException:
-        print('Interrupted, Exiting')
+        print ('Interrupted, Exiting')
         exit()
--- mirv_real/drive/drive.cpp
+++ mirv_real/drive/drive.cpp
@@ -9,10 +9,13 @@
 #include <thread>
 #include <SDL2/SDL.h>
 #include <unistd.h>
+#include <math.h>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Float32MultiArray.h"
 #include "sensor_msgs/Joy.h"
+#include "diagnostic_msgs/DiagnosticArray.h"
+#include "diagnostic_msgs/DiagnosticStatus.h"
 
 
 using namespace ctre::phoenix;
@@ -29,12 +32,148 @@ TalonFX frontLeftDrive(2, interface);
 TalonFX backLeftDrive(3, interface); 
 TalonFX backRightDrive(4, interface);
 
+void initializeDriveMotors(){
+	//PID config
+	float kP = 0.1;
+	float kI = 0.0;
+	float kD = 0.0;
+
+	frontRightDrive.Config_kP(0, kP);
+	frontRightDrive.Config_kI(0, kI);
+	frontRightDrive.Config_kD(0, kD);
+
+	frontLeftDrive.Config_kP(0, kP);
+	frontLeftDrive.Config_kI(0, kI);
+	frontLeftDrive.Config_kD(0, kD);
+
+	backLeftDrive.Config_kP(0, kP);
+	backLeftDrive.Config_kI(0, kI);
+	backLeftDrive.Config_kD(0, kD);
+
+	backRightDrive.Config_kP(0, kP);
+	backRightDrive.Config_kI(0, kI);
+	backRightDrive.Config_kD(0, kD);
+
+	//zero drive motors
+	while (frontRightDrive.GetSelectedSensorPosition() != 0.0){
+		frontRightDrive.SetSelectedSensorPosition(0.0);
+	}
+	while (frontLeftDrive.GetSelectedSensorPosition() != 0.0){
+		frontLeftDrive.SetSelectedSensorPosition(0.0);
+	}
+	while (backLeftDrive.GetSelectedSensorPosition() != 0.0){
+		backLeftDrive.SetSelectedSensorPosition(0.0);
+	}
+	while (backRightDrive.GetSelectedSensorPosition() != 0.0){
+		backRightDrive.SetSelectedSensorPosition(0.0);
+	}
+}
+
+//maximum rpm of drive motors
+double maxVelocity = 6380.0;
+
+//distance between right and left wheels (meters)
+//16 and 11/16 inches
+double wheelSpacing = 0.4238625;
+
+//wheel circumference (meters)
+double wheelCircumference = 0.608447957;
+
+//gear ratio of the drive motors
+double motorToWheelRatio = 12.0;
+
+//whether joystick is connected
+bool haveJoystick = true;
+
+//convert motor ticks to meters
+double getDistanceFromTicks(double ticks){
+	return (ticks / 2048.0) * wheelCircumference / motorToWheelRatio;
+}
+
+//pose object for returning odometry info
+class Pose {
+	public:
+	double x, y, angle;
+};
+
+class Odometry {
+	public:
+
+	//mirv pose
+	double x = 0.0;
+	double y = 0.0;
+	double angle = M_PI / 2.0;
+
+	//radius of circular path of travel
+	double r = 0.0;
+	//angular velocity of mirv around circular path of travel
+	double deltaTheta = 0.0;
+
+	//tick position from previous update
+	double prevLeftTicks = 0.0;
+	double prevRightTicks = 0.0;
+
+	void update(){
+		//get current tick positions
+		double leftTicks = (frontLeftDrive.GetSelectedSensorPosition() + backLeftDrive.GetSelectedSensorPosition()) / 2.0;
+		double rightTicks = -(frontRightDrive.GetSelectedSensorPosition() + backRightDrive.GetSelectedSensorPosition()) / 2.0;
+
+		//calculate how many ticks the motor moved by
+		double leftDisplacement = getDistanceFromTicks(leftTicks - prevLeftTicks);
+		double rightDisplacement = getDistanceFromTicks(rightTicks - prevRightTicks);
+		
+		if (leftDisplacement == rightDisplacement){
+			//radius calculation edge case
+			double displacement = rightDisplacement;
+
+			x += displacement * cos(angle);
+			y += displacement * sin(angle);
+		} else {
+			//calculate radius and change in theta
+			r = abs((wheelSpacing * (rightDisplacement + leftDisplacement)) / (2 * (rightDisplacement - leftDisplacement)));
+			
+			if (leftDisplacement > rightDisplacement){
+				deltaTheta = leftDisplacement / (r + (wheelSpacing / 2.0));
+				angle -= deltaTheta;
+			} else {
+				deltaTheta = rightDisplacement / (r + (wheelSpacing / 2.0));
+				angle += deltaTheta;
+			}
+
+			//update pose
+			x += (r * cos(deltaTheta) * sin(angle)) + (r * cos(angle) * sin(deltaTheta)) - (r * sin(angle));
+			y += (r * sin(deltaTheta) * sin(angle)) - (r * cos(angle) * cos(deltaTheta)) + (r * cos(angle));
+		}
+
+		prevLeftTicks = leftTicks;
+		prevRightTicks = rightTicks;
+	}
+
+	Pose getPose(){
+		Pose p;
+		p.x = x;
+		p.y = y;
+		p.angle = angle;
+		return p;
+	}
+
+	double getX(){
+		return x;
+	}
+	double getY(){
+		return y;
+	}
+	double getAngle(){
+		return angle;
+	}
+};
+
+Odometry odometry;
 
 // Set speed of individual motors
 void setDriveCallback(const std_msgs::Float32MultiArray::ConstPtr& msg){
 	ROS_INFO("I heard: [%f]", msg->data[0]);
 
-	//int len = *(&msg->data + 1) - &msg->data;
 	int len = 2;
 	if(len ==2){
 		int talonID = (int)msg->data[0];
@@ -55,30 +194,51 @@ void setDriveCallback(const std_msgs::Float32MultiArray::ConstPtr& msg){
 		
 		}
 	}
-	//ROS_INFO(msg->data);
 	
+}
 
-	//.Set(ControlMode::PercentOutput, 0.1);
-	
+void diagnosticCallback(const diagnostic_msgs::DiagnosticArray::ConstPtr& statusMsg){
+	/*
+	Check whether joystick is connected
+	If not, stop drive motors
+	*/
+	int level = statusMsg->status[0].level;
+	if (level == 0){
+		haveJoystick = true;
+	} else if (level == 2){
+		haveJoystick = false;
+		ROS_INFO("Jostick DISCONNECTED");
+		frontRightDrive.Set(ControlMode::PercentOutput, 0.0);
+		frontLeftDrive.Set(ControlMode::PercentOutput, 0.0);
+		backLeftDrive.Set(ControlMode::PercentOutput, 0.0);
+		backRightDrive.Set(ControlMode::PercentOutput, 0.0);
+	}
+	ROS_INFO("MIRV Pose - X: [%f] Y: [%f] Angle: [%f]", odometry.getX(), odometry.getY(), odometry.getAngle());
 }
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
-	float drive = joy->axes[1];
-	float turn = joy->axes[3];
-	float rightMotorPower = drive + turn;
-	float leftMotorPower = -drive + turn;
 
+	double drive = joy->axes[1];
+	double turn = joy->axes[3];
+	double rightMotorPower = -drive - turn;
+	double leftMotorPower = drive - turn;
 
-
-	ROS_INFO("Drive: [%f], Turn: [%f], Right Motors: [%f], Left Motors: [%f]", drive, turn, rightMotorPower, leftMotorPower);
+	//Turn motor percent to percent of maximum velocity
+	double rightMotorRPM = rightMotorPower * maxVelocity;
+	double leftMotorRPM = leftMotorPower * maxVelocity;
 	
+	//Convert percent of maximum velocity to tic per 100 ms
+	double rightMotorTP100MS = (rightMotorRPM / 600.0) * 2048.0;
+	double leftMotorTP100MS = (leftMotorRPM / 600.0) * 2048.0;
 
-	frontRightDrive.Set(ControlMode::PercentOutput, rightMotorPower);
-	frontLeftDrive.Set(ControlMode::PercentOutput, leftMotorPower);
-	backLeftDrive.Set(ControlMode::PercentOutput, leftMotorPower);
-	backRightDrive.Set(ControlMode::PercentOutput, rightMotorPower);
-
+	if (haveJoystick){
+		frontRightDrive.Set(ControlMode::Velocity, rightMotorTP100MS);
+		frontLeftDrive.Set(ControlMode::Velocity, leftMotorTP100MS);
+		backLeftDrive.Set(ControlMode::Velocity, leftMotorTP100MS);
+		backRightDrive.Set(ControlMode::Velocity, rightMotorTP100MS);
+ 	}
 }
+
 /** simple wrapper for code cleanup */
 void sleepApp(int ms)
 {
@@ -92,21 +252,16 @@ int main(int argc, char **argv) {
 
 	ros::Subscriber sub = n.subscribe("drive", 10, setDriveCallback);
 	ros::Subscriber joySub = n.subscribe("joy", 10, joyCallback);
+	ros::Subscriber diagnosticSub = n.subscribe("diagnostics", 10, diagnosticCallback);
   	//ros::spin();
+	
+	initializeDriveMotors();
 
 	while(ros::ok()){
 		ctre::phoenix::unmanaged::Unmanaged::FeedEnable(100);
+		odometry.update();
 		ros::spinOnce();
-		
 	}
-	//while (true) {
-	//	ctre::phoenix::unmanaged::Unmanaged::FeedEnable(100);
-	//	
-	//	sleepApp(20);
-	//}
-	
-	
-	
- 	
+  	
 	return 0;
 }
