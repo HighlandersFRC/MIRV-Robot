b6e1d8273453415d55ab6d388ef397e2e303fa7a
lukesta182 <lskhan@xemaps.com>
2022-06-10 15:42:06 -0600

    fixed as much as possible

---
+++ mirv_control/PurePursuit/purePursuit.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+import math
+import numpy as np
+from numpy import array
+import time
+import rospy
+from std_msgs.msg import Float64MultiArray
+
+class PurePursuit():
+    wheelBaseWidth = 0.438
+    nextPointDistanceDec = 0
+    maxDriveSpeed = 1
+    lookAheadDist = 5
+    allowedError = 0.05
+    robotCordList = []
+    cordList = []
+    currentTruckCord = [0,0]
+    pub = rospy.Publisher("VelocityDrive", Float64MultiArray, queue_size = 10)
+
+    def __init__(self):
+        rospy.init_node('PurePursuitController', anonymous=True)
+
+    ##updates truck coordinate targets to rover cordites as rover moves
+    def UpdateTargetPoints(self):
+        newTruckCord = self.getTruckCord()
+        theta = newTruckCord[2] - 3.14159/2
+        xBRef = newTruckCord[0]
+        yBRef = newTruckCord[1]
+        length = 0
+        for i in range(len(self.robotCordList)):
+            xTBody = self.cordList[i][0]*math.cos(theta) + self.cordList[i][1]*math.sin(theta) - yBRef*math.sin(theta) - xBRef*math.cos(theta)
+            yTBody = -self.cordList[i][0]*math.sin(theta) + self.cordList[i][1]*math.cos(theta) - yBRef*math.cos(theta) + xBRef*math.sin(theta)
+            distToPoint = round(math.sqrt(math.pow(xTBody,2) + math.pow(yTBody,2)), 3)
+            self.robotCordList[i] = [xTBody, yTBody, distToPoint]
+    ## helper methods for update target points
+    def getTruckCord(self):
+        return self.currentTruckCord
+
+
+    def getPath(self):
+        pathList = [[10, 0]]
+        for point in pathList:
+            self.cordList.append([point[0],point[1],0])
+            self.robotCordList.append([point[0],point[1],0])
+        self.UpdateTargetPoints()
+        self.nextPointDistanceDec = self.robotCordList[0][2]
+
+
+
+    def removeTargetPoint(self):
+        if (abs(self.robotCordList[0][2]) < self.nextPointDistanceDec):
+            self.nextPointDistanceDec = self.robotCordList[0][2]
+        elif (abs(self.robotCordList[0][2]) > self.lookAheadDist and abs(self.robotCordList[0][2]) > (self.nextPointDistanceDec+0.5) and abs(self.robotCordList[0][2]) < (self.lookAheadDist+ (1/4)*self.lookAheadDist )):
+            self.robotCordList.pop(0)
+            self.cordList.pop(0)
+            if(self.robotCordList):
+                self.nextPointDistanceDec = self.robotCordList[0][2]
+            else:
+                return True
+        return False
+
+    ## calulates wheel velocity's for that given frame
+    def calculateSpeedSide(self, maxSpeed, x, y, la):
+        leftVel = maxSpeed
+        rightVel = maxSpeed
+        if(abs(x)>self.allowedError):
+            cRad = self.generateRadius(x, la)
+            iCirc = self.generateCircumference((cRad - (self.wheelBaseWidth/2)))
+            oCirc = self.generateCircumference((cRad + (self.wheelBaseWidth/2)))
+            innerSpeedRatio = (iCirc/oCirc)
+            turnRight = True
+            if(x>=0):  
+                rightVel = rightVel*innerSpeedRatio
+                print("turningRight")
+            else:
+                leftVel = leftVel*innerSpeedRatio
+                print("turningLeft")
+        return ([leftVel, rightVel])
+
+    ##helper methods to calculateSpeedSides
+    def generateRadius(self,x,la):
+        return(math.pow(la,2)/(abs(2*x)))
+    def generateCircumference(self, rad):
+        return(2*3.141592*rad)
+
+
+
+    ##determines the target cord for each snapshot
+    def getTargetCordAndDriveSpeed(self,la, maxSpeed):
+        closePoint = self.closestOut(la)
+        farPoint = self.furthestIn(la)
+        targetPoint = []
+        snapshotLa = la
+        if (closePoint and farPoint):
+            targetPoint = self.doubleVectorIntercept(closePoint, farPoint,la)
+        elif farPoint:
+            targetPoint = farPoint
+            snapshotLa = (targetPoint[0]**2 + targetPoint[1]**2)**0.5
+            if (snapshotLa < self.allowedError):
+                return ("atDest")
+        elif closePoint:
+            targetPoint = self.vectorIntercept(closePoint, la)
+        
+        else:
+            print("cannot determine target point")
+        sideSpeeds = self.calculateSpeedSide(maxSpeed, targetPoint[0], targetPoint[1], snapshotLa)
+        return([sideSpeeds,targetPoint])
+
+    ##helper method for getTargetCord
+    def vectorIntercept(self, closePoint, la):
+        cPO = array(closePoint)
+        normalVect = (cPO)/(((cPO**2).sum()**0.5))
+        targetPoint = normalVect*la
+        print("Vector Intercept")
+        return targetPoint
+    def doubleVectorIntercept(self, closePoint, farPoint, la):
+        cPO = array(closePoint)
+        fPI = array(farPoint)
+        zeroVect= cPO-fPI
+        prodOfMag = ((fPI**2).sum()**0.5)*((zeroVect**2).sum()**0.5)
+        gama = 3.141592 - math.acos(((zeroVect.dot(fPI))/prodOfMag))
+        zeta = math.asin((((fPI**2).sum()**0.5)*math.sin(gama))/(la))
+        theta = 3.1415-gama-zeta
+        distToEdge = la*(math.sin(theta))/math.sin(gama)
+        unitP2Vect = ((cPO-fPI)/(((cPO-fPI)**2).sum()**0.5))
+        p2EVect = distToEdge*unitP2Vect
+        target = p2EVect+fPI
+        print("double Vector Intercept")
+        return target
+    def closestOut(self, la):
+        closePoint = []
+        for point in self.robotCordList:
+            if(point[2] > la):
+                return [point[0], point[1]]
+        return closePoint
+    def furthestIn(self, la):
+        farPoint = []
+        for point in self.robotCordList:
+            if(point[2] <= la):
+                farPoint = point.copy()
+            else:
+                if(farPoint):
+                    return [farPoint[0], farPoint[1]]
+        return farPoint
+
+    def callbackOdom(self, data):
+        self.getPath()
+        if (cordList):
+            self.currentTruckCord = data
+            self.UpdateTargetPoints()
+            output = self.getTargetCordAndDriveSpeed(self.lookAheadDist, self.MaxDriveSpeed)
+            isFinished = self.removeTargetPoint()
+            if(output != "atDest" and not isFinished):
+                self.pub.publish([output[0][0], output[0][1]])
+                ## ros.drive.setvel(output[0][0], output[0][1])
+            else:
+                self.pub.publish([0, 0])
+                ## ros.drive.setvel(0,0)
+        else:
+            self.pub.publish([0, 0])
+            ## ros.drive.setvel(0,0)
+            print("no target Point")
+    
+    
+    def run(self):
+        self.getPath()
+        rospy.subscriber("odometry/encoder", Float64MultiArray, self.callbackOdom())
+        rospy.spin()
+
+
+if __name__ == '__main__':
+    controller = PurePursuit()
+    controller.run()
+   
