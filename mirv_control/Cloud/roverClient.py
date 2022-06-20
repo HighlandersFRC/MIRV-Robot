76f215e8a67a6095f846a11c9869ff790123ffad
Paul-Mick <paulmickrobotics@gmail.com>
2022-06-20 12:08:29 -0600

    Created rover api client and startup launch file

---
+++ mirv_control/Cloud/roverClient.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import socketio
+import rospy
+from std_msgs.msg import Float64
+import os
+
+HOST = os.getenv("API_URL")
+
+sio = socketio.Client()
+
+def sendData(data):
+    sio.emit("data", data)
+
+def battery_voltage_callback(voltage):
+    print("sending voltage")
+    sendData({"battery-voltage": voltage.data})
+
+def run():
+    rospy.init_node("roverClient")
+    try:
+        sio.connect(f"{HOST}/ws", headers={"roverID": "mirv_1"}, auth={"password": None}, socketio_path="/ws/socket.io")
+    except:
+        print("Could not connect")
+    battery_voltage_sub = rospy.Subscriber("battery/voltage", Float64, battery_voltage_callback)
+    rospy.spin()
+
+if __name__ == "__main__":
+    run()
