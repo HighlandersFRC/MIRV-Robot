98b634c1a06473dd44146acabe98b2a663b577d3
dory <dory@mirv.com>
2022-06-23 16:45:51 -0400

    fixing catkin_make issues

--- mirv_real/Camera/tools/cameraPublisher.py
+++ mirv_real/Camera/tools/cameraPublisher.py
@@ -8,54 +8,54 @@ import os, sys
 import shutil
 import time
 from pathlib import Path
-from faster_RCNN import get_faster_rcnn_resnet
+#from faster_RCNN import get_faster_rcnn_resnet
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
-import torch
-import torch.backends.cudnn as cudnn
-from numpy import random
-import numpy as np
+#import torch
+#import torch.backends.cudnn as cudnn
+#from numpy import random
+#import numpy as np
 
-from typing import Optional, List, Tuple
-from dataclasses import field
+#from typing import Optional, List, Tuple
+#from dataclasses import field
 
-from lib.config import cfg
-from lib.config import update_config
-from lib.utils.utils import create_logger, select_device, time_synchronized
-from lib.models import get_net
-from lib.dataset import LoadImages, LoadStreams
-from lib.core.general import bbox_iou, non_max_suppression, scale_coords
-from lib.utils import plot_one_box,show_seg_result
-from lib.core.function import AverageMeter
-from lib.core.postprocess import morphological_process, connect_lane
-from tqdm import tqdm
+#from lib.config import cfg
+#from lib.config import update_config
+#from lib.utils.utils import create_logger, select_device, time_synchronized
+#from lib.models import get_net
+#from lib.dataset import LoadImages, LoadStreams
+#from lib.core.general import bbox_iou, non_max_suppression, scale_coords
+#from lib.utils import plot_one_box,show_seg_result
+#from lib.core.function import AverageMeter
+#from lib.core.postprocess import morphological_process, connect_lane
+#from tqdm import tqdm
 import depthai
 
-from faster_RCNN import get_faster_rcnn_resnet
-from transformations import ComposeDouble
-from transformations import ComposeSingle
-from transformations import FunctionWrapperDouble
-from transformations import FunctionWrapperSingle
-from transformations import apply_nms, apply_score_threshold
-from transformations import normalize_01
+#from faster_RCNN import get_faster_rcnn_resnet
+#from transformations import ComposeDouble
+#from transformations import ComposeSingle
+#from transformations import FunctionWrapperDouble
+#from transformations import FunctionWrapperSingle
+#from transformations import apply_nms, apply_score_threshold
+#from transformations import normalize_01
 
-from backbone_resnet import ResNetBackbones
-from PIL import Image
+#from backbone_resnet import ResNetBackbones
+#from PIL import Image
 
 from numpy import asarray
 
 # import fiftyone as fo
 
-import matplotlib as plt
-import torchvision.transforms as transforms
+#import matplotlib as plt
+#import torchvision.transforms as transforms
 
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+#from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
-import torchvision
+#import torchvision
 
-import torch.multiprocessing as mp
+#import torch.multiprocessing as mp
 import rospy
 from rospy.numpy_msg import numpy_msg
 from rospy_tutorials.msg import Floats
