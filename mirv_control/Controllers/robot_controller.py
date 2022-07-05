5c2d5d91f773dfe2a1089ba03eeeb050312d2471
AdarshPay <adarsh.payyakkil@gmail.com>
2022-07-05 16:16:02 -0400

    Changed import for PID

--- mirv_control/Controllers/robot_controller.py
+++ mirv_control/Controllers/robot_controller.py
@@ -2,9 +2,7 @@
 import math
 import rospy
 from std_msgs.msg import Float64MultiArray, String, Float64
-import os
-os.chdir("src/MIRV-Robot/mirv_control")
-import controlLoops.PID as PID
+import PID
 
 class RobotController:
     def __init__(self):
