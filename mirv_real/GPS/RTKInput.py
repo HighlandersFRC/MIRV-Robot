d1e130c3e425baf2c010a6e7cc3ec487d1b7df6e
Luke <luke@neaeraconsulting.com>
2022-07-11 13:41:19 -0600

    merge with dev

--- mirv_real/GPS/RTKInput.py
+++ mirv_real/GPS/RTKInput.py
@@ -13,33 +13,29 @@ rospy.init_node('RTKModule', anonymous=True)
 currentGGA = GGAData()
 currentVTG = VTGData()
 
-with serial.Serial('/dev/ttyACM9', 115200, timeout=1) as ser:
-    while True:
-== == == =
 print("reading in data")
 with serial.Serial('/dev/ttyUSB2', 115200, timeout=1) as ser:
     while not rospy.is_shutdown():
->>>>>> > odometry
-   try:
-        line = ser.readline().decode('utf-8')  # read a '\n' terminated line
-        line = line.split(",")
-    except:
-        pass
-    try:
-        if (line[0] == "$GPVTG"):
-            currentVTG.loadMessage(line)
-        if (line[0] == "$GPGGA"):
-            currentGGA.loadMessage(line)
-            rosPubMsg.header.stamp = rospy.Time.now()
-            rosPubMsg.header.frame_id = "map"
-            rosPubMsg.latitude = currentGGA.getLatitude()
-            rosPubMsg.longitude = currentGGA.getLongitude()
-            rosPubMsg.altitude = currentGGA.getAltitude()
-            rosPubMsg.status.status = currentGGA.getQualityIndicator()
-            rosPubMsg.position_covariance = currentGGA.getCovariance()
-            pub.publish(rosPubMsg)
-            print(line)
-    except KeyboardInterrupt:
-        break
-    except:
-        print("Failed to receive message, trying again in 1 second")
+        try:
+            line = ser.readline().decode('utf-8')  # read a '\n' terminated line
+            line = line.split(",")
+        except:
+            pass
+        try:
+            if (line[0] == "$GPVTG"):
+                currentVTG.loadMessage(line)
+            if (line[0] == "$GPGGA"):
+                currentGGA.loadMessage(line)
+                rosPubMsg.header.stamp = rospy.Time.now()
+                rosPubMsg.header.frame_id = "map"
+                rosPubMsg.latitude = currentGGA.getLatitude()
+                rosPubMsg.longitude = currentGGA.getLongitude()
+                rosPubMsg.altitude = currentGGA.getAltitude()
+                rosPubMsg.status.status = currentGGA.getQualityIndicator()
+                rosPubMsg.position_covariance = currentGGA.getCovariance()
+                pub.publish(rosPubMsg)
+                print(line)
+        except KeyboardInterrupt:
+            break
+        except:
+            print("Failed to receive message, trying again in 1 second")
