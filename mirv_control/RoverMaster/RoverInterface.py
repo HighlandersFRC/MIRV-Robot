c071e4f152e59fa520cadb4e6552aa89f7dcd498
lukesta182 <lskhan@xemaps.com>
2022-07-18 10:48:53 -0600

    shuifter client struct

--- mirv_control/RoverMaster/RoverInterface.py
+++ mirv_control/RoverMaster/RoverInterface.py
@@ -12,45 +12,39 @@ def convertToOneD(TwoDArray):
     for point in TwoDArray:
         pass
 
+class RoverInterface():
+    def __init__(self):
+        self.PPclient = actionlib.SimpleActionClient('PurePursuitAS', mirv_control.msg.PurePursuitAction)
+        self.PPclient.wait_for_server()
+        print("connected to Pure Pursuit Server")
 
+    def PP_client(self, targetPoint):
+        mirv_control.msg.PurePursuitGoal.TargetPoints = [5,0]
+        mirv_control.msg.PurePursuitGoal.NumTargetPoints = 1
+        goal = mirv_control.msg.PurePursuitGoal
+        # Sends the goal to the action server.
+        client.send_goal(goal)
 
-def fibonacci_client():
-    # Creates the SimpleActionClient, passing the type of the action
-    # (FibonacciAction) to the constructor.
-    client = actionlib.SimpleActionClient('PurePursuitAS', mirv_control.msg.PurePursuitAction)
+        # Waits for the server to finish performing the action.
+        client.wait_for_result()
+        print(client.get_result())
+        time.sleep(10)
+        mirv_control.msg.PurePursuitGoal.TargetPoints = [3,-2]
+        mirv_control.msg.PurePursuitGoal.NumTargetPoints = 1
+        goal = mirv_control.msg.PurePursuitGoal
+        # Sends the goal to the action server.
+        client.send_goal(goal)
 
-    # Waits until the action server has started up and started
-    # listening for goals.
-    client.wait_for_server()
-
-    # Creates a goal to send to the action server.
-    mirv_control.msg.PurePursuitGoal.TargetPoints = [5,0]
-    mirv_control.msg.PurePursuitGoal.NumTargetPoints = 1
-    goal = mirv_control.msg.PurePursuitGoal
-    # Sends the goal to the action server.
-    client.send_goal(goal)
-
-    # Waits for the server to finish performing the action.
-    client.wait_for_result()
-    print(client.get_result())
-    time.sleep(10)
-    mirv_control.msg.PurePursuitGoal.TargetPoints = [3,-2]
-    mirv_control.msg.PurePursuitGoal.NumTargetPoints = 1
-    goal = mirv_control.msg.PurePursuitGoal
-    # Sends the goal to the action server.
-    client.send_goal(goal)
-
-    # Waits for the server to finish performing the action.
-    client.wait_for_result()
-    # Prints out the result of executing the action
-    print(client.get_result())  # A FibonacciResult
+        # Waits for the server to finish performing the action.
+        client.wait_for_result()
+        # Prints out the result of executing the action
+        print(client.get_result())  # A FibonacciResult
 
 if __name__ == '__main__':
     try:
         # Initializes a rospy node so that the SimpleActionClient can
         # publish and subscribe over ROS.
-        rospy.init_node('fibonacci_client_py')
-        result = fibonacci_client()
-        print(result)
+        rospy.init_node('Master_client_py')
+        RoverInterface().PP_client
     except rospy.ROSInterruptException:
         print("program interrupted before completion", file=sys.stderr)
