b617841f5e00b3129609505001a932fd0dbce834
lukesta182 <lskhan@xemaps.com>
2022-07-27 13:21:00 -0600

    fixes for pi-lit controller

--- start.sh
+++ start.sh
@@ -4,12 +4,6 @@ sudo ip link set can0 type can bitrate 1000000
 sudo ifconfig can0 up
 sudo ifconfig can0 txqueuelen 1000
 
-echo "seting up enviornment variables"
-source set_envs.sh
-source setenv.sh
-echo "set up enviornment variables"
-#roslaunch mirv_real mirv.launch
-
 function exposeDI() {
     echo $1 >/sys/class/gpio/export 2>/dev/null
     echo in >/sys/class/gpio/gpio$1/direction
@@ -26,4 +20,11 @@ exposeDO 320
 exposeDO 321
 exposeDO 322
 exposeDO 323
+
+echo "seting up enviornment variables"
+source set_envs.sh
+source setenv.sh
+echo "set up enviornment variables"
+#roslaunch mirv_real mirv.launch
+
 
