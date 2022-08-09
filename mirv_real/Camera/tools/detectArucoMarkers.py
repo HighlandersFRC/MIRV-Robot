d30d98802022e2dc7f6e2b86c1b7d1f0b2aca8d4
dory <dory@mirv.com>
2022-08-09 17:47:36 -0400

    Undocking sequence without calibration

--- mirv_real/Camera/tools/detectArucoMarkers.py
+++ mirv_real/Camera/tools/detectArucoMarkers.py
@@ -1,55 +1,16 @@
 #!/usr/bin/env python3
 from cmath import pi
 import math
-import queue
-import threading
 import cv2
-import argparse
 import os, sys
-import shutil
 import time
-from pathlib import Path
-from faster_RCNN import get_faster_rcnn_resnet
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
-import torch
-import torch.backends.cudnn as cudnn
-from numpy import random
 import numpy as np
-
-from typing import Optional, List, Tuple
-from dataclasses import field
-
-from lib.config import cfg
-from lib.config import update_config
-from lib.utils.utils import create_logger, select_device, time_synchronized
-from lib.models import get_net
-from lib.dataset import LoadImages, LoadStreams
-from lib.core.general import bbox_iou, non_max_suppression, scale_coords
-from lib.utils import plot_one_box,show_seg_result
-from lib.core.function import AverageMeter
-from lib.core.postprocess import morphological_process, connect_lane
-from tqdm import tqdm
 import depthai
 
-from faster_RCNN import get_faster_rcnn_resnet
-from transformations import ComposeDouble
-from transformations import ComposeSingle
-from transformations import FunctionWrapperDouble
-from transformations import FunctionWrapperSingle
-from transformations import apply_nms, apply_score_threshold
-from transformations import normalize_01
-
-from backbone_resnet import ResNetBackbones
-from PIL import Image
-
-from numpy import asarray
-
-# import fiftyone as fo
-
-import matplotlib as plt
 import torchvision.transforms as transforms
 
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
@@ -60,10 +21,7 @@ import torch.multiprocessing as mp
 
 import roslib
 import rospy
-from rospy_tutorials.msg import Floats
-from rospy.numpy_msg import numpy_msg
 from std_msgs.msg import Float64MultiArray
-from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import ros_numpy
 from mirv_control.msg import depth_and_color_msg as depthAndColorFrame
