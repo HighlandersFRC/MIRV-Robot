5fd24e0c68716130e00d245acb9d179c48b843e9
dory <dory@mirv.com>
2022-08-17 13:50:01 -0400

    Adedd in pi-lit placement code

--- mirv_control/RoverMaster/RoverMacros.py
+++ mirv_control/RoverMaster/RoverMacros.py
@@ -64,30 +64,30 @@ class roverMacros():
         self.interface.loadPointToSQL("deploy", side)
 
     def placeAllPiLits(self, firstPoint, roughHeading, lane_width, formation_type):
-
-
+        self.interface.changeNeuralNetworkSelected("lanes")
         firstPointTruckCoord = self.interface.CoordConversion_client_goal(firstPoint)
         startingTarget = [firstPointTruckCoord]
-        #self.interface.PP_client_goal(startingTarget)
-        #points = self.interface.getPlacementPoints(firstPoint, roughHeading)
-        points = placement.generate_pi_lit_formation(firstPoint, roughHeading, lane_width, formation_type)
+        self.interface.PP_client_goal(startingTarget)
+        detected_lanes = {'right': (firstPoint[0], firstPoint[1])}
+        points = self.interface.getPlacementPoints(firstPoint, roughHeading)
+        points = placement.generate_pi_lit_formation(detected_lanes, roughHeading, lane_width, formation_type)
+        print(points)
+
         self.interface.changeNeuralNetworkSelected("none")
         intakeSide = "switch_right"
 
         for pnt  in points:
             point = self.interface.CoordConversion_client_goal(pnt)
             target = [point]
-
-            # self.interface.PP_client_goal(target)
-            # self.placePiLitFromSide(6, intakeSide)
-            # self.interface.intake_command_pub.publish(String("reset"))
-            # self.interface.loadPointToSQL("deploy", intakeSide)
-            # if(intakeSide == "switch_right"):
-            #     intakeSide = "switch_left"
-            # else:
-            #     intakeSide = "switch_right"
-            # time.sleep(2)
-        time.sleep(30)
+            self.interface.PP_client_goal(target)
+            self.placePiLitFromSide(6, intakeSide)
+            self.interface.intake_command_pub.publish(String("reset"))
+            self.interface.loadPointToSQL("deploy", intakeSide)
+            if(intakeSide == "switch_right"):
+                intakeSide = "switch_left"
+            else:
+                intakeSide = "switch_right"
+            time.sleep(2)
         return True
         
 
